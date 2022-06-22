b7e7feae0ebcdab8189668624ff5151a9d45283a
Vinicius <vinicius.deoliveira@outlook.com>
2022-06-22 19:27:13 -0300

    Remove unecessary javascript

--- app/javascript/controllers/imports.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("#sortable-headers").sortable({
-    update: function(event, ui) {
-      var sortedIDs = $("#sortable-headers").sortable("toArray");
-
-      $('input#import_headers').val(sortedIDs)
-    }
-  });
-});
