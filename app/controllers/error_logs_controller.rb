6d78db568adde253c148fbe3c51af185e43dadec
Vinicius <vinicius.deoliveira@outlook.com>
2022-06-22 22:22:30 -0300

    Show contacts file columns on error logs page

--- app/controllers/error_logs_controller.rb
+++ app/controllers/error_logs_controller.rb
@@ -2,9 +2,9 @@
 
 class ErrorLogsController < ApplicationController
   def index
-    import = current_user.imports.find(params[:import_id])
+    @import = current_user.imports.find(params[:import_id])
     @error_logs = current_user.error_logs
-                              .where(import: import)
+                              .where(import: @import)
                               .order(created_at: :desc)
                               .page(params[:page])
   end
